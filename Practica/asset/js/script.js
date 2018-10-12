e17e225bc60319e9b880784a1cbde12fb9f04b79
feranstirman1 <00024117@uca.edu.sv>
2018-10-12 10:24:35 -0600

    funciones terminadas

--- Practica/asset/js/script.js
+++ Practica/asset/js/script.js
@@ -1,5 +1,27 @@
-window.addEventListener("load",()=>{
-    
 
+var container;
+container= document.getElementById("c1");
+container.innerText="Hola Mundo!";
+container.style.backgroundColor="#000000";
+container.style.color="#1cb723";
+container.style.width="200px";
+container.style.height="200px";
+container.innerHTML="<input type='text' placeholder='escriba su texto'>";
 
+var containers= document.querySelectorAll(".c2");
+containers.forEach(element=>{
+    element.style.backgroundColor= "#009999";
 });
+containers.innerHTML="<input type='text' placeholder='Escriba su texto'>";
+
+var boton= document.getElementById("btn-click");
+boton.onclick=function(evt){
+    alert("HOLA MUNDO");
+};
+var btnCopy= document.getElementById("btnCopy");
+btnCopy.onclick = function(evt) { 
+    let msj = document.getElementById('textMsj').value;
+    document.getElementById('showMsj').innerText = msj;
+};
+
+
